4aee144bd9940419a23d8cb0eaed3e2f980a82e9
omuricer <guts.inaq@gmail.com>
2019-04-07 18:21:30 +0900

    akari

---
+++ app/src/index.js
@@ -0,0 +1,6 @@
+import './sass/common.scss';
+import {MDCTopAppBar} from '@material/top-app-bar/index';
+
+// Instantiation
+const topAppBarElement = document.querySelector('.mdc-top-app-bar');
+const topAppBar = new MDCTopAppBar(topAppBarElement);
