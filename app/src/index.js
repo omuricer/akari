52016df72a1026f73717b1801c994d01e944a340
omuricer <guts.inaq@gmail.com>
2019-09-17 10:03:35 +0900

    mobile めにゅううううう

--- app/src/index.js
+++ app/src/index.js
@@ -4,21 +4,18 @@ import { MDCRipple } from "@material/ripple";
 
 import Swiper from "swiper";
 
-// Instantiation
-const topAppBarElement = document.querySelector(".mdc-top-app-bar");
-if (topAppBarElement) {
-  const topAppBar = new MDCTopAppBar(topAppBarElement);
-}
-
-const buttonElements = document.querySelectorAll(".mdc-button");
-if (buttonElements) {
-  let a = Array.prototype.slice.call(buttonElements);
-  a.forEach(element => {
-    new MDCRipple(element);
-  });
-}
-
 window.onload = function() {
+  // Instantiation
+  const topAppBarElement = document.querySelector(".mdc-top-app-bar");
+  if (topAppBarElement) {
+    const topAppBar = new MDCTopAppBar(topAppBarElement);
+  }
+
+  // Ripple for button
+  actMDCRipple(".mdc-button");
+  actMDCRipple(".menu.mobile a");
+
+  // Init swiper
   var swiper = new Swiper(".swiper-container", {
     effect: "fade",
     loop: true,
@@ -46,6 +43,26 @@ window.onload = function() {
       }
     }
   });
-  // new Swiper(".swiper-container");
+
+  // menu
+  const menuOpen = document.querySelector(".menu__open");
+  menuOpen.addEventListener("click", function() {
+    const menu = document.querySelector(".menu.mobile__inner");
+    menu.style.display="block";
+  });
+  const menuClose = document.querySelector(".menu__close");
+  menuClose.addEventListener("click", function() {
+    const menu = document.querySelector(".menu.mobile__inner");
+    menu.style.display="none";
+  });
 };
 
+function actMDCRipple(selector) {
+  let elements = document.querySelectorAll(".mdc-button");
+  if (elements) {
+    elements = Array.prototype.slice.call(elements, 0);
+    elements.forEach(function(e) {
+      new MDCRipple(e);
+    });
+  }
+}
