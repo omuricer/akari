486a7111879d048a11ccab482237d8eeb575b370
omuricer <guts.inaq@gmail.com>
2020-04-26 10:12:16 +0900

    newsを追加

--- docs/app-65847d67fd104f49b6e9.js
+++ docs/app-65847d67fd104f49b6e9.js
@@ -4753,15 +4753,19 @@ function dom7_modular_scroll() {
 
 
 // CONCATENATED MODULE: ./node_modules/swiper/dist/js/swiper.esm.bundle.js
-function _possibleConstructorReturn(self, call) { if (call && (_typeof(call) === "object" || typeof call === "function")) { return call; } return _assertThisInitialized(self); }
-
-function _getPrototypeOf(o) { _getPrototypeOf = Object.setPrototypeOf ? Object.getPrototypeOf : function _getPrototypeOf(o) { return o.__proto__ || Object.getPrototypeOf(o); }; return _getPrototypeOf(o); }
-
-function _assertThisInitialized(self) { if (self === void 0) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return self; }
-
 function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function"); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, writable: true, configurable: true } }); if (superClass) _setPrototypeOf(subClass, superClass); }
 
 function _setPrototypeOf(o, p) { _setPrototypeOf = Object.setPrototypeOf || function _setPrototypeOf(o, p) { o.__proto__ = p; return o; }; return _setPrototypeOf(o, p); }
+
+function _createSuper(Derived) { return function () { var Super = _getPrototypeOf(Derived), result; if (_isNativeReflectConstruct()) { var NewTarget = _getPrototypeOf(this).constructor; result = Reflect.construct(Super, arguments, NewTarget); } else { result = Super.apply(this, arguments); } return _possibleConstructorReturn(this, result); }; }
+
+function _possibleConstructorReturn(self, call) { if (call && (_typeof(call) === "object" || typeof call === "function")) { return call; } return _assertThisInitialized(self); }
+
+function _assertThisInitialized(self) { if (self === void 0) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return self; }
+
+function _isNativeReflectConstruct() { if (typeof Reflect === "undefined" || !Reflect.construct) return false; if (Reflect.construct.sham) return false; if (typeof Proxy === "function") return true; try { Date.prototype.toString.call(Reflect.construct(Date, [], function () {})); return true; } catch (e) { return false; } }
+
+function _getPrototypeOf(o) { _getPrototypeOf = Object.setPrototypeOf ? Object.getPrototypeOf : function _getPrototypeOf(o) { return o.__proto__ || Object.getPrototypeOf(o); }; return _getPrototypeOf(o); }
 
 function swiper_esm_bundle_classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
@@ -7809,6 +7813,8 @@ var extendedDefaults = {};
 var swiper_esm_bundle_Swiper = /*#__PURE__*/function (_SwiperClass) {
   _inherits(Swiper, _SwiperClass);
 
+  var _super = _createSuper(Swiper);
+
   function Swiper() {
     var _this;
 
@@ -7831,7 +7837,7 @@ var swiper_esm_bundle_Swiper = /*#__PURE__*/function (_SwiperClass) {
     if (!params) params = {};
     params = Utils.extend({}, params);
     if (el && !params.el) params.el = el;
-    _this = _possibleConstructorReturn(this, _getPrototypeOf(Swiper).call(this, params));
+    _this = _super.call(this, params);
     Object.keys(prototypes).forEach(function (prototypeGroup) {
       Object.keys(prototypes[prototypeGroup]).forEach(function (protoMethod) {
         if (!Swiper.prototype[protoMethod]) {
@@ -8798,7 +8804,7 @@ var Keyboard$1 = {
 
 function isEventSupported() {
   var eventName = 'onwheel';
-  var isSupported = eventName in doc;
+  var isSupported = (eventName in doc);
 
   if (!isSupported) {
     var element = doc.createElement('div');
