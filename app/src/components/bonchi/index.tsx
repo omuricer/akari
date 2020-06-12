78f27042db0f02877c836b5d8183e6fea90df823
omuricer <guts.inaq@gmail.com>
2020-06-12 10:42:54 +0900

    shopコンポーネントに引数をついｋ

--- app/src/components/bonchi/index.tsx
+++ app/src/components/bonchi/index.tsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 
 import Shop from "@/components/bonchi/shop";
 
-const shops = [
+const shopDefinitions = [
   {
     image: "image/bonchi/shop1.png",
     position: { x: 10, y: 10 },
@@ -17,19 +17,18 @@ const shops = [
   },
 ];
 
-const generateRondomDelay = (): number => {
-  return roundAtDigit(Math.random() * 6, 2);
-};
-const roundAtDigit = (num: number, digit): number => {
-  // num = 1.2345 & digit = 2 --> 1.23
-  return Math.floor(num * Math.pow(10, digit)) / Math.pow(10, digit);
-};
-
 interface IBonchiProps {}
 const Bonchi: React.FC<IBonchiProps> = (props) => {
+  var shopElements: JSX.Element[] = [];
+  shopDefinitions.map((s) =>
+    shopElements.push(
+      <Shop positionX={s.position.x} positionY={s.position.y} image={s.image} />
+    )
+  );
+
   return (
     <div className="mdc-layout-grid no-margin bonchi">
-      <div className="mdc-layout-grid__inner content"></div>
+      <div className="mdc-layout-grid__inner content">{shopElements}</div>
     </div>
   );
 };
--- app/src/components/bonchi/shop.tsx
+++ app/src/components/bonchi/shop.tsx
@@ -12,9 +12,18 @@ const Shop: React.FC<IShopProps> = (props) => {
         top: `${props.positionY}px`,
         left: `${props.positionX}px`,
         backgroundImage: props.image,
-        animationDelay: `${delaySec}s`,
+        animationDelay: `${generateRondomDelay()}s`,
       }}
     />
   );
 };
+
+const generateRondomDelay = (): number => {
+  return roundAtDigit(Math.random() * 6, 2);
+};
+const roundAtDigit = (num: number, digit: number): number => {
+  // num = 1.2345 & digit = 2 --> 1.23
+  return Math.floor(num * Math.pow(10, digit)) / Math.pow(10, digit);
+};
+
 export default Shop;
