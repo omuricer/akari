a804f6155e836d15be5cc301862d338d9f449b1f
omuricer <guts.inaq@gmail.com>
2020-06-27 19:49:26 +0900

    スマホ版、ようこそダイアログを追加

--- app/src/components/fukushimaBonchi/index.tsx
+++ app/src/components/fukushimaBonchi/index.tsx
@@ -4,6 +4,13 @@ import { TShop, Shops } from "@/components/fukushimaBonchi/shops";
 import Shop from "@/components/fukushimaBonchi/shop";
 import TemplateHero from "@/components/template/templateHero";
 import Grid from "@material-ui/core/Grid";
+import useMediaQuery from "@material-ui/core/useMediaQuery";
+import { useTheme } from "@material-ui/core/styles";
+import Dialog from "@material-ui/core/Dialog";
+import DialogActions from "@material-ui/core/DialogActions";
+import DialogContent from "@material-ui/core/DialogContent";
+import Button from "@material-ui/core/Button";
+import { Typography, FormHelperText } from "@material-ui/core";
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -26,8 +33,28 @@ const useStyles = makeStyles((theme: Theme) =>
   })
 );
 
+const detectBreakPoint = () => {
+  const theme = useTheme();
+  type breakPoint = "xs" | "sm" | "md" | "lg";
+  const breakPoints: breakPoint[] = ["xs", "sm", "md", "lg"];
+
+  let detectedBreakPoint: null | breakPoint = null;
+  breakPoints.forEach((breakPoint: breakPoint) => {
+    if (useMediaQuery(theme.breakpoints.up(breakPoint))) {
+      detectedBreakPoint = breakPoint;
+      return;
+    }
+  });
+  return detectedBreakPoint ?? "lg";
+};
+const isMobile = () => {
+  const breakPoint = detectBreakPoint();
+  return breakPoint == "xs" || breakPoint == "sm";
+};
+
 interface IFukushimaBonchiProps {}
 const FukushimaBonchi: React.FC<IFukushimaBonchiProps> = (props) => {
+  const [open, setOpen] = useState<boolean>(true);
   const classes = useStyles();
 
   var shopElements: JSX.Element[] = [];
@@ -35,6 +62,39 @@ const FukushimaBonchi: React.FC<IFukushimaBonchiProps> = (props) => {
     shopElements.push(<Shop shop={s} key={i} />)
   );
 
+  const initDialog: JSX.Element = isMobile() ? (
+    <Dialog open={open}>
+      <DialogContent
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <Typography>FUKUSHIMA BONCHI へようこそ！</Typography>
+        <Typography style={{ marginTop: "1rem" }}>
+          画面を縦と横にスクロールしながら
+          <br />
+          FUKUSHIMA BONCHI を散策してください！
+        </Typography>
+      </DialogContent>
+      <DialogActions>
+        <Button
+          variant="outlined"
+          size="large"
+          onClick={() => {
+            setOpen(false);
+          }}
+          autoFocus
+          style={{ width: "110px" }}
+        >
+          Close
+        </Button>
+      </DialogActions>
+    </Dialog>
+  ) : (
+    <div />
+  );
   return (
     <React.Fragment>
       <TemplateHero>
@@ -44,6 +104,7 @@ const FukushimaBonchi: React.FC<IFukushimaBonchiProps> = (props) => {
           </div>
         </Grid>
       </TemplateHero>
+      {initDialog}
     </React.Fragment>
   );
 };
