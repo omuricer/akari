6bafcd5da488238514aba46ff384fef972d40af5
omuricer <guts.inaq@gmail.com>
2020-07-01 23:05:51 +0900

    blank

--- app/src/index.tsx
+++ app/src/index.tsx
@@ -2,14 +2,13 @@ import React from "react";
 import { render } from "react-dom";
 
 import Theme from "@/components/theme";
-import Teaser from "@/components/fukushimaBonchi/teaser";
 import FukushimaBonchi from "@/components/fukushimaBonchi";
 import Entrance from "@/components/fukushimaBonchi/entrance";
 
 import "@/sass/index.scss";
 import "@/css/destyle.css";
 
-// TODO: index_old.jsの中身をコンポーネントに移行する
+// TODO: index_old.jsの中身をコンポーネントに移行する!
 require("./index_old.js");
 
 let target = document.getElementById("bonchi");
