24acc103ebc25ae090a880b75082ec716dbbcb30
omuricer <guts.inaq@gmail.com>
2020-10-15 19:59:59 +0900

    Youtubeライブがプレビュー表示されるように対応（暫定）

--- app/src/components/fukushimaBonchi/youtubeDialog.tsx
+++ app/src/components/fukushimaBonchi/youtubeDialog.tsx
@@ -9,10 +9,14 @@ import PlayView from "@/components/youtube/playView";
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
-    root: {},
+    root: {
+      width: "80vw",
+      [theme.breakpoints.down("xs")]: {
+        width: "95vw",
+      },
+    },
     contentRoot: {
       background: "rgba(225,225,225, 0.9)", // 透過を子要素に継承しないためRGBで指定している
-      minWidth: "600px",
       display: "flex",
       justifyContent: "center",
       alignItems: "center",
@@ -49,29 +53,31 @@ const YoutubeDialog: React.FC<IYoutubeDialogProps> = (props) => {
    */
   const searchMovies = async (apiProps: { key: string; channelId: string }) => {
     YoutubeAPI.init(apiProps.key);
-    const result = await YoutubeAPI.search({
+    return await YoutubeAPI.search({
       part: "snippet",
       type: "video",
       eventType: "live",
       maxResults: 1,
       channelId: apiProps.channelId,
     });
-    console.log(result);
   };
 
   useEffect(() => {
     const asyncs = async () => {
-      await searchMovies({
+      const movies = await searchMovies({
         key: "AIzaSyCOdHoXvxi2fferydOqo4O4Z-6o4p0s0fE",
-        channelId: "UCdZsC9AS5zcZEKOm6eIAC5w",
+        channelId: "UCGCZAYq5Xxojl_tSXcVJhiQ",
       });
+      setPlayVideoId(movies ? movies[0].videoId : undefined);
     };
     asyncs();
   }, []);
 
   return (
     <Dialog open={props.open} onClose={props.onClose} className={classes.root}>
-      <DialogContent className={classes.contentRoot}>aaaa</DialogContent>
+      <DialogContent className={classes.contentRoot}>
+        <PlayView videoId={playVideoId} />
+      </DialogContent>
       <DialogActions className={classes.actionsRoot}>
         <Button
           variant="outlined"
