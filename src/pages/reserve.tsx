a444315091fb7f69c1c5b2d69725ddc701a96f66
omuricer <guts.inaq@gmail.com>
2021-02-16 11:43:31 +0900

    usecaseをjson化

--- src/pages/reserve.tsx
+++ src/pages/reserve.tsx
@@ -5,59 +5,13 @@ import SubSubTitle from "components/subSubTitle";
 import UseCase from "components/useCase";
 import FadeOnScroll from "components/fadeOnScroll";
 import { UseCase as UseCaseModel } from "domain/model/useCase";
+import UseCaseData from "json/usecase";
 import Image from "next/image";
-
-const data: UseCaseModel[] = [
-  {
-    image: "/image/news/maturi_ato.jpg",
-    resource: "Akari park",
-    text:
-      "『アカリ夏マツリ』と題して、地域のショップをお招きしお祭りが開催されました！広場の一角ではワークショップも。写真掲載していますのでご覧ください。",
-    more: {
-      url:
-        "https://www.facebook.com/pg/yamorisyamomonone2018/photos/?tab=album&album_id=407041663275450&ref=page_internal",
-      text: "詳しくみる",
-    },
-  },
-  {
-    image: "/image/news/ima_ba2.jpg",
-    resource: "Studio",
-    text:
-      "『いま、地域に求められる場とは？』と題して、福島県内各地で豊かなコミュニティを生み出している人たちをお招きし、なぜそんな“場”をつくったの？運営体制はどのようにして実現させている？彼らが生み出す“場”の魅力とは？などについてお聞きできるイベントが開催されました。",
-    more: {
-      url:
-        "https://www.facebook.com/pg/yamorisyamomonone2018/photos/?tab=album&album_id=377448239568126&ref=page_internal",
-      text: "詳しくみる",
-    },
-  },
-  {
-    image: "/image/news/kaigi1.jpg",
-    resource: "Studio",
-    text:
-      "『ふくしまの暮らし方、働き方会議#1』と題して、もっと違う暮らし方、働き方、自分が触れてこなかった別の世界を考えるトークセッションが開催されました。",
-    more: {
-      url:
-        "https://www.facebook.com/pg/yamorisyamomonone2018/photos/?tab=album&album_id=291734231472861&ref=page_internal",
-      text: "詳しくみる",
-    },
-  },
-  {
-    image: "/image/news/marche.jpg",
-    resource: "Akari Park",
-    text:
-      "『空想マルシェ』と題して、１日限定のマルシェが誕生しました。みんなの空想を持ち寄って、理想のまちを自分の手でつくろう！を合言葉に多くの方々がご来場されました。",
-    more: {
-      url:
-        "https://www.facebook.com/pg/yamorisyamomonone2018/photos/?tab=album&album_id=267465137233104&ref=page_internal",
-      text: "詳しくみる",
-    },
-  },
-];
 
 export const getStaticProps: GetStaticProps<Props> = async ({ params }) => {
   const useCases = await (async () => {
     return new Promise<UseCaseModel[]>((resolve) => {
-      resolve(data);
+      resolve(UseCaseData);
     });
   })();
   return {
